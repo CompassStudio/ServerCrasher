18e302bc2b2328f26ba69326b77fc77f59725f07
H3xadecimal <h3xadecimal@afternode.cn>
2023-08-14 11:53:03 +0800

    Get good get commons-io

---
+++ src/main/java/xyz/zihaoxu/utils/FileUtil.java
@@ -0,0 +1,20 @@
+package xyz.zihaoxu.utils;
+
+import org.apache.commons.io.IOUtils;
+import xyz.zihaoxu.Main;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+
+public class FileUtil {
+    public static void saveResource(File target, String path) throws IOException {
+        try (InputStream stream = Main.class.getClassLoader().getResourceAsStream(path)) {
+            BufferedOutputStream bos = new BufferedOutputStream(Files.newOutputStream(target.toPath()));
+            bos.write(IOUtils.toByteArray(stream));
+            bos.close();
+        }
+    }
+}
